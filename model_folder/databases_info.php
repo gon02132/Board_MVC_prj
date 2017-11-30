1c32fc1e60d85cd0ed49ddccce253cde680c698d
Jin-SK <gon02132@naver.com>
2017-11-30 10:17:42 +0900

    remove

--- model_folder/databases_info.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD","autoset");
-define("DB_NAME", "jsk_test");
-define("TABLE_NAME","new_board");
-define("USERS_TABLE","sign_up_board");
-?>
